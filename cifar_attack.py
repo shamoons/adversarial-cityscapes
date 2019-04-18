664826a2512734e98acfe894bad5b499547233bc
Shamoon Siddiqui <shamoon@develop.io>
2019-04-18 10:39:57 -0400

    Saving adversarial examples works

--- cifar_attack.py
+++ cifar_attack.py
@@ -1,70 +1,58 @@
+from cifar_model import Model
 from cleverhans.attacks import FastGradientMethod
 from cleverhans.utils_keras import KerasModelWrapper
-
-from keras.datasets import cifar10
-from cifar_model import Model
-
 from keras import backend as K
-import tensorflow as tf
-import png
+from keras.datasets import cifar10
 import numpy as np
+import os
+import keras
+import imageio
+import tensorflow as tf
 
 num_classes = 10
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
 
 x_test_tensor = K.variable(x_test)
 
 model = Model(input_shape=x_train.shape[1:], num_classes=num_classes).model
 wrap = KerasModelWrapper(model)
 
-fgsm_params = {'eps': 0.3,
-               'clip_min': 0.,
-               'clip_max': 1.
-               #    'y_target': np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-               }
+target = keras.utils.to_categorical([0], num_classes)
+target = np.repeat(target, 10000, axis=0)
 
-i = 0
-fgsm = FastGradientMethod(wrap)
-adv = fgsm.generate(x_test_tensor, **fgsm_params)
+fgsm_params = {
+    'eps': 0.05,
+    'clip_min': 0.,
+    'clip_max': 1.,
+    'y_target': target
+}
 
-for adv_x in tf.unstack(adv):
-    img = tf.cast(adv_x, dtype=tf.uint8)
-    tf_image = tf.image.encode_jpeg(img)
-    tf.write_file('adversarial_examples/' + str(i) + '.jpg', tf_image)
-    i += 1
-
-quit()
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     fgsm = FastGradientMethod(wrap, sess=sess)
     adv = fgsm.generate(x_test_tensor, **fgsm_params)
-    print(adv)
-    print(sess.run(adv))
-    quit()
 
-# sess = tf.InteractiveSession()
-# print(adv.eval())
-# quit()
+    print('Done creating examples')
 
+    i = 0
+    for adv_x in tf.unstack(adv):
+        print('Saving: ', i)
+        asnumpy = sess.run(adv_x)
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    asnumpy = sess.run(adv)
+        asnumpy *= 255
+        asnumpy = asnumpy.astype('uint8')
 
-    print(asnumpy)
-    # for i, image in enumerate(asnumpy):
-    #     png.from_array(image).save("adversarial_examples/" + i + ".png")
+        original_image = x_test[i] * 255
+        original_image = original_image.astype('uint8')
 
-# # print(adv)
-# i = 0
-# for adv_x in tf.unstack(adv):
+        imageio.imwrite("adversarial_examples/" + str(i) + ".adv.png", asnumpy)
+        imageio.imwrite("adversarial_examples/" +
+                        str(i) + ".png", original_image)
 
-# with tf.Session() as sess:
-#     sess.run(tf.global_variables_initializer())
-#     asnumpy = sess.run(tf.unstack(adv_x))
-#     print(adv_x)
-#     asnumpy = tf.unstack(adv_x)
-#     print(asnumpy)
-#     png.from_array(asnumpy).save("adversarial_examples/" + i + ".png")
-#     i += 1
+        i += 1
--- cifar_sample.py
+++ cifar_sample.py
@@ -1,4 +1,23 @@
 import keras
+import numpy as np
 
 from keras.datasets import cifar10
 from cifar_model import Model
+
+num_classes = 10
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+model = Model(input_shape=x_test.shape[1:], num_classes=num_classes).model
+
+y_test_hat = model.evaluate(model, x=x_test, y=y_test)
+
+# y_test_hat = np.argmax([y_test_hat])
+
+print(y_test_hat)
+# print(y_test)
+
+# for i in range(len(x_test)):
+#     mo
+#     print(x_test[i])
+#     print(y_test[i])
